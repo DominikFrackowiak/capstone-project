c66f0a69165110000778c2d5d5e59b6d4dac0fda
DominikFrackowiak <rorschach@op.pl>
2023-05-11 11:52:17 +0200

    navigation done, routing done

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+import './Navigation.scss'
+
+const Navigation = () => {
+	return (
+		<>
+			<div className='navigation'>
+				<div>
+					<Link className='nav-link' to={'/'}>
+						<Logo />
+					</Link>
+				</div>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to={'/shop'}>
+						SHOP
+					</Link>
+				</div>
+			</div>
+
+			<Outlet />
+		</>
+	)
+}
+
+export default Navigation
---
+++ src/routes/shop/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+ return (
+  <div>
+   I'm your shop
+  </div>
+ );
+};
+
+export default Shop;
